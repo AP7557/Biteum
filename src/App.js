4b8a7a38c8aa202c8d98e145773b9f31108f9948
Akash Patel <ap.akash.patel1999+github@gmail.com>
2021-10-31 23:07:46 -0400

    changed the api method name

--- src/App.js
+++ src/App.js
@@ -21,63 +21,67 @@ function App() {
    *                ExchangeLink: 'link name'
    *              },
    *  }, ...] */
-  const [data, setData] = useState({ Bitcoin: {}, Ethereum: {} });
+  const [cryptoData, setCryptoData] = useState({ Bitcoin: {}, Ethereum: {} });
 
   //Get data from server
-  const apiCall = () => {
-    const tempData = { Bitcoin: {}, Ethereum: {} };
+  const getApiData = () => {
     //API call to server to get BTCs data, and then put that data
     //along with its logo in Bitcoin object in temp data
     axios
       .get('/getBitcoinData')
       .then((response) => response.data)
-      .then(
-        (data) =>
-          (tempData.Bitcoin = {
-            ...data,
-            CryptoLogo: 'bitcoin1.png',
-            CryptoColor: '#FFC300',
-          })
-      )
+      .then((data) => {
+        setCryptoData((prev) => {
+          return {
+            ...prev,
+            Bitcoin: {
+              ...data,
+              CryptoLogo: 'bitcoin1.png',
+              CryptoColor: '#FFC300',
+            },
+          };
+        });
+      })
       .catch((error) => {
         console.log(error);
       });
 
     //API call to server to get ETHs data, and then put that data
     //along with its logo in Ethereum object in temp data
-    // axios
-    //   .get('/getEthereumData')
-    //   .then((response) => response.data)
-    //   .then(
-    //     (data) =>
-    //       (tempData.Ethereum = {
-    //         ...data,
-    //         CryptoLogo: 'ethereum1.png',
-    //         CryptoColor: '#FF8E00',
-    //       })
-    //   )
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
-
-    //Put the temp data in the state
-    setData({ Bitcoin: tempData.Bitcoin, Ethereum: tempData.Ethereum });
+    axios
+      .get('/getEthereumData')
+      .then((response) => response.data)
+      .then((data) => {
+        setCryptoData((prev) => {
+          return {
+            ...prev,
+            Ethereum: {
+              ...data,
+              CryptoLogo: 'ethereum1.png',
+              CryptoColor: '#FF8E00',
+            },
+          };
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   useEffect(() => {
     //Initial call to API
-    apiCall();
+    getApiData();
     //Call API every 65 seconds as we can only make 10 api call in 60 seconds
-    const interval = setInterval(() => {
-      apiCall();
-    }, 65000);
-    return () => clearInterval(interval);
+    // const interval = setInterval(() => {
+    //   getApiData();
+    // }, 65000);
+    // return () => clearInterval(interval);
   }, []);
 
   return (
     <MainPage>
-      <Cards data={data.Bitcoin} />
-      <Cards data={data.Ethereum} />
+      <Cards data={cryptoData.Bitcoin} />
+      <Cards data={cryptoData.Ethereum} />
     </MainPage>
   );
 }
